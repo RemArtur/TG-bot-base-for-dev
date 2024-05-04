104d69a4b470edda669592592f61a06b40cd8db2
Artur Galeev <artgaleev007@gmail.com>
2024-05-04 16:13:00 +0300

    small change

--- commands.py
+++ commands.py
@@ -2,7 +2,9 @@
 
 from aiogram.types import BotCommand
 
-admin_commands = [BotCommand(command="admin", description="Панель администратора")]
+admin_commands = [
+    BotCommand(command="admin", description="Панель администратора")
+]
 
 user_commands = [
     BotCommand(command="start", description="Начало работы"),
